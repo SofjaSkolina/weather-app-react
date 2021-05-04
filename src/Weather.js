f0c8eff0e383c2ac72d8d9fef47cdf949513799c
SofjaSkolina <sofjaskolina@gmail.com>
2021-05-04 21:52:57 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -8,5 +8,5 @@ export default function Weather (props){
 let apiKey = "edab5056ac17d7bbccfb78a16eafdf10";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-  return (<h1>hi</h1>);
+  return (<h1>Hello from react</h1>);
 }
