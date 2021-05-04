6d1fde5063efbbac792bf694cd56d1b40c51feac
SofjaSkolina <sofjaskolina@gmail.com>
2021-05-04 21:44:16 +0200

    added axios integration

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from 'axios';
+
+export default function Weather (props){
+    function handleResponse(response){
+        alert(`The wather in ${props.city} is ${response.data.main.temp}`)
+    }
+let apiKey = "edab5056ac17d7bbccfb78a16eafdf10";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  return (<h1>hi</h1>);
+}
